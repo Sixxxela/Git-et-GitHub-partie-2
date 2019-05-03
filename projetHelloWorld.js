c1984f7f6dd89cbb2e5407c39b94de14eb19991c
Sixela <alexochocincoo@gmail.com>
2019-05-03 13:04:44 -0400

    Ajout de code dans projetHelloWolrd.js

---
+++ projetHelloWorld.js
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        //Print the words Hello Worlds on the screen
+        System.out.println("Hello World");
+       int myLength = "Hello World".length();
+  System.out.println(myLength);
+          
+    }
+    
+
+    
+}
